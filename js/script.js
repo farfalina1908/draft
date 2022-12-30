ba48618ed7ab6753debc915195771cd209ba2c22
Elena Berezina <farfalina1908@gmail.com>
2022-12-30 17:38:04 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
    $('.slider').slick({
     arrows:false,
     dots: true,
+  speed:500,
+  easing:'ease',
+  infinite:false,
+autoplay:true,
+autoplaySpeed:2000,
+pauseOnFocus:true,
+pauseOnHover:true,
+pauseOnDotsHover:true,
    });
     
 })
